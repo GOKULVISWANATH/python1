7877565140f6b8a08a8884ff07747ed98e9313c2
GOKULVISWANATH <43375943+GOKULVISWANATH@users.noreply.github.com>
2018-10-15 17:12:59 +0530

    Update set3-24.py

--- set3-24.py
+++ set3-24.py
@@ -1,6 +1,9 @@
 x=[]
 y=int(input("value:"))
-for i in range(y):
-    y=int(input("list:"))
-    x.append(y)
-    print(sorted(x))
+if(1<=y<=1000):
+    for i in range(y):
+        y=int(input("list:"))
+        x.append(y)
+        print(sorted(x))
+else:
+    print(0)
